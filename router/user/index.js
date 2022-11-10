92edf4702d44fcf9caa8a5f9d77acc546421a465
JaeWonLee0303 <dlwodnjs3003@gmail.com>
2022-11-10 15:55:02 +0900

    11_10_classend

--- router/user/index.js
+++ router/user/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const postRouter = require("./post/index");
-const userRouter = require("./user");
+
 const router = express.Router();
 
-router.use("/post", postRouter);
-router.use("/user", userRouter);
+router.get("/", (req, res) => {
+  res.send("asdfasdfasdf post");
+});
 
 module.exports = router;
+
+// 하위 디렉토리 조회용 재귀 함수
